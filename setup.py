1bda016d76fb626d514ca4b8e8b54a2ef3f7bc2d
Saad <saadfast.qc@gmail.com>
2017-01-04 22:08:41 +0500

    Added dist

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sub-merge',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='saad',
+    author_email='saad_lah@hotmail.com',
+    description='Merge and sync subtitles with video'
+)
